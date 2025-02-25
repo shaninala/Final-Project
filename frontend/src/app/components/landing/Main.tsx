4641a294becac438d1fde9a74b6312f2593743eb
michaelzheng02 <michaelz113@hotmail.com>
2025-02-25 12:02:13 -0500

    chore: create Main.tsx for landing page

---
+++ frontend/src/app/components/landing/Main.tsx
@@ -0,0 +1,13 @@
+export default function Main() {
+  return (
+    <section>
+      <h1>Welcome to GasHub</h1>
+      <p>
+        At GasHub, we believe you deserve to save on every tank. Discover the
+        best gas prices near you in just a few taps, so you can keep more money
+        in your pocket and hit the road with confidence. Let us help you fuel
+        your journey for less!
+      </p>
+    </section>
+  );
+}
