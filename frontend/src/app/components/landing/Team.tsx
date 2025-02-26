fbf19f0592355b86861c9b2df9364e2399405b68
michaelzheng02 <michaelz113@hotmail.com>
2025-02-26 18:10:12 -0500

    feat: start Team section for landing page (WIP)

---
+++ frontend/src/app/components/landing/Team.tsx
@@ -0,0 +1,14 @@
+import Image, { StaticImageData } from "next/image";
+import Link from "next/link";
+
+export default function Team() {
+  return (
+    <section>
+      <span>Meet our Team</span>
+      <span>
+        We're a group of motivated individuals, fueled by a shared passion for
+        what we do.
+      </span>
+    </section>
+  );
+}
