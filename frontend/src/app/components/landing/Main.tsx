9cf59851ef6b62a9a0d06129ad9ae5edf6acd24f
michaelzheng02 <michaelz113@hotmail.com>
2025-02-25 19:46:20 -0500

    style: apply CSS styles in Main.tsx

--- frontend/src/app/components/landing/Main.tsx
+++ frontend/src/app/components/landing/Main.tsx
@@ -1,8 +1,10 @@
 export default function Main() {
   return (
-    <section>
-      <h1>Welcome to GasHub</h1>
-      <p>
+    <section className="min-h-screen flex flex-col justify-center items-center py-20 px-4 text-center bg-[#FAF3E0]">
+      <h1 className="text-6xl sm:text-7xl md:text-8xl lg:text-9xl font-bold text-[#3D405B] mb-8">
+        Welcome to GasHub
+      </h1>
+      <p className="text-xl sm:text-3xl md:text-4xl text-[#3D405B] mb-8 max-w-6xl mx-auto">
         At GasHub, we believe you deserve to save on every tank. Discover the
         best gas prices near you in just a few taps, so you can keep more money
         in your pocket and hit the road with confidence. Let us help you fuel
