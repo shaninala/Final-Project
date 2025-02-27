008f8e315697ee81b779156c6b5e6959779e1a70
michaelzheng02 <michaelz113@hotmail.com>
2025-02-26 21:02:22 -0500

    feat: added styles to Team section and integrate GitHub and LinkedIn components

--- frontend/src/app/components/landing/Team.tsx
+++ frontend/src/app/components/landing/Team.tsx
@@ -2,6 +2,8 @@ import Image, { StaticImageData } from "next/image";
 import Link from "next/link";
 import michael_photo from "./images/michael_photo.jpg";
 import shani_photo from "./images/shani_photo.jpg";
+import GitHub from "./social/GitHub";
+import LinkedIn from "./social/Linkedin";
 
 type Member = {
   name: string;
@@ -30,27 +32,38 @@ const teamMembers: Member[] = [
 
 export default function Team() {
   return (
-    <section>
-      <span>Meet our Team</span>
-      <span>
+    <section
+      id="team"
+      className="min-h-screen flex flex-col justify-center py-20 px-4 bg-[#FAF3E0]"
+    >
+      <span className="block text-4xl sm:text-6xl md:text-7xl lg:text:8xl font-bold text-[#3D405B] text-center mb-10">
+        Meet our Team
+      </span>
+      <span className="block text-center text-xl sm:text-xl md:text-2xl text-[#3D405B] mb-16 max-w-7xl mx-auto">
         We're a group of motivated individuals, fueled by a shared passion for
         what we do.
       </span>
-      <div>
+      <div className="flex flex-wrap justify-center gap-48">
         {teamMembers.map((member, index) => (
-          <div key={index}>
-            <div>
+          <div key={index} className="text-center">
+            <div className="mb-4">
               <Image
                 src={member.image}
                 alt={member.name}
                 width={350}
                 height={350}
+                className="rounded-full mx-auto"
               />
             </div>
-            <span>{member.role}</span>
-            <div>
-              {member.githubUrl}
-              {member.linkedinUrl}
+            <span className="block text-xl sm:text-2xl md:text-3xl font-bold text-[#3D405B] mb-1">
+              {member.name}
+            </span>
+            <span className="block text-sm sm:text-md md:text-xl font-semibold text-[#3D405B] mb-3">
+              {member.role}
+            </span>
+            <div className="flex items-center justify-center space-x-10 mt-2">
+              {member.githubUrl && <GitHub url={member.githubUrl} />}
+              {member.linkedinUrl && <LinkedIn url={member.linkedinUrl} />}
             </div>
           </div>
         ))}
