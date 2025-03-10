c5d46a7b9f96ad4085c531034489339f3422858c
michaelzheng02 <michaelz113@hotmail.com>
2025-03-09 23:46:33 -0400

    style: adding styling to the gas page form

--- frontend/src/app/components/gas/Form.tsx
+++ frontend/src/app/components/gas/Form.tsx
@@ -9,12 +9,20 @@ export default function Form() {
   const [zipCode, setZipCode] = useState<string>("");
   const [fuelType, setFuelType] = useState<string>("Regular");
   const [selfService, setSelfService] = useState<string>("Yes");
+
   return (
     <>
-      <form>
-        <h1>Search Gas Station</h1>
-        <div>
-          <label>Zip Code/City:</label>
+      <form className="space-y-6">
+        <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold text-[#3D405B] mb-8 text-center">
+          Search Gas Station
+        </h1>
+        <div className="space-y-3">
+          <label
+            htmlFor="zipCode"
+            className="text-sm sm:text-md md:text-lg lg:text-xl block text-[#3D405B] font-semibold"
+          >
+            Zip Code/City:
+          </label>
           <Input
             id="zipCode"
             value={zipCode}
@@ -24,9 +32,14 @@ export default function Form() {
             placeholder="Search gas stations by city or zip code..."
           />
         </div>
-        <div>
-          <div>
-            <label>Fuel Type</label>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <div className="space-y-3">
+            <label
+              htmlFor="fuelType"
+              className="text-sm sm:text-md md:text-lg lg:text-xl block text-[#3D405B] font-semibold"
+            >
+              Fuel Type
+            </label>
             <Select
               id="fuelType"
               value={fuelType}
@@ -36,19 +49,24 @@ export default function Form() {
               options={["Regular", "Midgrade", "Premium", "Diesel"]}
             />
           </div>
+          <div className="space-y-3">
+            <label
+              htmlFor="selfService"
+              className="text-sm sm:text-md md:text-lg lg:text-xl block text-[#3D405B] font-semibold"
+            >
+              Self-Service
+            </label>
+            <Select
+              id="selfService"
+              value={selfService}
+              onChange={(event: ChangeEvent<HTMLSelectElement>) =>
+                setSelfService(event.target.value)
+              }
+              options={["Yes", "No"]}
+            />
+          </div>
         </div>
-        <div>
-          <label>Self-Service</label>
-          <Select
-            id="selfService"
-            value={selfService}
-            onChange={(event: ChangeEvent<HTMLSelectElement>) =>
-              setSelfService(event.target.value)
-            }
-            options={["Yes", "No"]}
-          />
-        </div>
-        <div>
+        <div className="flex justify-center pt-4">
           <Button type="submit">Search Gas Station</Button>
         </div>
       </form>
