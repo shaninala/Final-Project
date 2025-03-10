21789782f00720637a72001b43a9488fd78ddc02
michaelzheng02 <michaelz113@hotmail.com>
2025-03-09 23:14:54 -0400

    feat: add input, button, and select components into the search gas page

--- frontend/src/app/components/gas/Form.tsx
+++ frontend/src/app/components/gas/Form.tsx
@@ -1,20 +1,52 @@
+"use client";
+
+import { useState, type ChangeEvent, type FormEvent } from "react";
 import Button from "./Button";
+import Input from "./Input";
+import Select from "./Select";
 
 export default function Form() {
+  const [zipCode, setZipCode] = useState<string>("");
+  const [fuelType, setFuelType] = useState<string>("Regular");
+  const [selfService, setSelfService] = useState<string>("Yes");
   return (
     <>
       <form>
         <h1>Search Gas Station</h1>
         <div>
           <label>Zip Code/City:</label>
+          <Input
+            id="zipCode"
+            value={zipCode}
+            onChange={(event: ChangeEvent<HTMLInputElement>) =>
+              setZipCode(event.target.value)
+            }
+            placeholder="Search gas stations by city or zip code..."
+          />
         </div>
         <div>
           <div>
             <label>Fuel Type</label>
+            <Select
+              id="fuelType"
+              value={fuelType}
+              onChange={(event: ChangeEvent<HTMLSelectElement>) =>
+                setFuelType(event.target.value)
+              }
+              options={["Regular", "Midgrade", "Premium", "Diesel"]}
+            />
           </div>
         </div>
         <div>
           <label>Self-Service</label>
+          <Select
+            id="selfService"
+            value={selfService}
+            onChange={(event: ChangeEvent<HTMLSelectElement>) =>
+              setSelfService(event.target.value)
+            }
+            options={["Yes", "No"]}
+          />
         </div>
         <div>
           <Button type="submit">Search Gas Station</Button>
