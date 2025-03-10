6fa612dd19f483885be9cd62196ae00840947550
michaelzheng02 <michaelz113@hotmail.com>
2025-03-09 23:44:47 -0400

    style: update styling for gas page

---
+++ frontend/src/app/gas/page.tsx
@@ -0,0 +1,13 @@
+import Form from "../components/gas/Form";
+
+export default function Gas() {
+  return (
+    <div className="min-h-screen flex flex-col bg-[#FAF3E0]">
+      <div className="flex-1 bg-[#FAF3E0] p-6 pt-[200px]">
+        <div className="max-w-3xl mx-auto">
+          <Form />
+        </div>
+      </div>
+    </div>
+  );
+}
