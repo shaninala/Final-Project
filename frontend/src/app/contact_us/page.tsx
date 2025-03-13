aef6950b8f45df0c120450034cb895a7738ffa31
shaninala <shani88904@gmail.com>
2025-03-13 16:23:50 -0400

    Updated page.tsx with new changes

--- frontend/src/app/contact_us/page.tsx
+++ frontend/src/app/contact_us/page.tsx
@@ -1,13 +1,9 @@
 import Contact from "../components/contact_us/Contact";
-import Navigation from "../components/landing/Navigation";
 
 export default function Home() {
   return (
     <>
-      <Navigation />
-      <main>
-        <Contact />
-      </main>
+      <Contact />
     </>
   );
 }
