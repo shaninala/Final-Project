db5497a11ac37a371de1f22f89f64ebe12426846
michaelzheng02 <michaelz113@hotmail.com>
2025-03-28 22:46:30 -0400

    feat: add mobile responsivness to input, select, and gas result

--- frontend/src/app/components/gas/Input.tsx
+++ frontend/src/app/components/gas/Input.tsx
@@ -9,7 +9,8 @@ export default function Input({ id, ...props }: InputProps) {
     <input
       id={id}
       type="text"
-      className="bg-[#E8D9B5] border border-[#C2A885] w-full p-5 focus:outline-none focus:ring-4 focus:ring-[#C2A885]"
+      className="bg-[#E8D9B5] border border-[#C2A885] w-full p-3 sm:p-4 md:p-5 text-sm sm:text-base focus:outline-none focus:ring-4 focus:ring-[#C2A885] rounded"
+      required
       {...props}
     />
   );
