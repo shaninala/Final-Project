b28fefe5b0f6fecb9bdc77ca6f6e7755edfbb18e
michaelzheng02 <michaelz113@hotmail.com>
2025-04-07 20:33:43 -0400

    feat: add ability to redirect to new page with more details

--- frontend/src/app/components/gas/GasStationCard.tsx
+++ frontend/src/app/components/gas/GasStationCard.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 interface GasStationProps {
   id: number;
   name: string;
@@ -15,6 +17,7 @@ interface GasStationProps {
 }
 
 export default function GasStationCard({
+  id,
   name,
   address,
   city,
@@ -24,36 +27,30 @@ export default function GasStationCard({
   price,
 }: GasStationProps) {
   return (
-    <div className="bg-[#E8D9B5] rounded-lg p-4 sm:p-6 mb-4 shadow-md">
-      <div className="flex justify-between items-start">
-        <div>
-          <span className="text-lg sm:text-xl font-semibold text-[#3D405B]">
-            {name}
-          </span>
-          <p className="text-sm sm:text-base">{address}</p>
-          <p className="text-sm sm:text-base">{`${city}, ${zipCode}`}</p>
-        </div>
-      </div>
-      <div className="mt-3">
-        <div>
+    <Link href={`/gas/${id}`}>
+      <div className="bg-[#E8D9B5] rounded-lg p-4 sm:p-6 mb-4 shadow-md cursor-pointer">
+        <div className="flex justify-between items-start">
           <div>
-            <span></span>
-            <ul className="grid grid-cols-2 sm:grid-cols-3 gap-2">
-              {fuelTypes.map((fuelType, index) => {
-                const priceKey = fuelType.toLowerCase() as keyof typeof price;
-                return (
-                  <li
-                    key={index}
-                    className="text-sm sm:text-base text-[#3D405B]"
-                  >
-                    {fuelType}: ${price[priceKey]?.toFixed(2) || "N/A"}
-                  </li>
-                );
-              })}
-            </ul>
+            <span className="text-lg sm:text-xl font-semibold text-[#3D405B]">
+              {name}
+            </span>
+            <p className="text-sm sm:text-base">{address}</p>
+            <p className="text-sm sm:text-base">{`${city}, ${zipCode}`}</p>
           </div>
         </div>
+        <div className="mt-3">
+          <ul className="grid grid-cols-2 sm:grid-cols-3 gap-2">
+            {fuelTypes.map((fuelType, index) => {
+              const priceKey = fuelType.toLowerCase() as keyof typeof price;
+              return (
+                <li key={index} className="text-sm sm:text-base text-[#3D405B]">
+                  {fuelType}: ${price[priceKey]?.toFixed(2) || "N/A"}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
