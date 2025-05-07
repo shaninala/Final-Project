ef965d134c81ab383d5bc822646ff2aa3b7a37ad
shaninala <shani88904@gmail.com>
2025-05-07 12:51:53 -0400

    delete old backend files

--- backend/routes/geocodeRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getCoordinatesByZip } = require('../controllers/geocodeController');
-
-// Route: GET /api/geocode?zipCode=12345 example
-router.get('/', getCoordinatesByZip);
-
-module.exports = router;
